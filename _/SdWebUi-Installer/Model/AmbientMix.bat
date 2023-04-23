0012f3f04a79e91655b8896ac520947214397124
Rinne <rinne@noreply.com>
2023-04-23 13:46:16 +0900

    fix ambient mix bug

--- _/SdWebUi-Installer/Model/AmbientMix.bat
+++ _/SdWebUi-Installer/Model/AmbientMix.bat
@@ -4,7 +4,7 @@ pushd %~dp0..\..\..\SdWebUi\Model\
 echo AmbientMix.safetensors: https://huggingface.co/OedoSoldier/ambientmix/
 if not exist Anime\ ( mkdir Anime\ )
 if not exist Anime\AmbientMix.safetensors (
-	curl -Lo IlluAnimest\AmbientMix.safetensors^
+	curl -Lo Anime\AmbientMix.safetensors^
 	https://huggingface.co/OedoSoldier/ambientmix/resolve/main/ambientmix-half.safetensors
 )
 
