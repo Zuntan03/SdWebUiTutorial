16e84c25c191d843c4616e9ae39f58b0b81b2755
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-04-28 22:07:32 +0900

    bug fix

--- _/SdWebUi-Installer/Lora/Situ/JapaneseToilet.bat
+++ _/SdWebUi-Installer/Lora/Situ/JapaneseToilet.bat
@@ -4,7 +4,7 @@ pushd %~dp0..\..\..\..\SdWebUi\Lora\
 echo JapaneseToilet.safetensors: https://civitai.com/models/9495/japanese-toilet
 if not exist Situ\ ( mkdir Situ\ )
 if not exist Situ\JapaneseToilet.safetensors (
-	curl -Lo Situ\AnalBeJapaneseToiletads.safetensors^
+	curl -Lo Situ\JapaneseToilet.safetensors^
 	https://civitai.com/api/download/models/11270
 	timeout /t 10 /nobreak
 )
