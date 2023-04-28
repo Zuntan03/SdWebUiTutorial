7d82d617925edfab93a0c45ef28106dc1bbdf157
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-04-28 15:25:46 +0900

    remove timer

--- _/SdWebUi-DownloadModelAndLora.bat
+++ _/SdWebUi-DownloadModelAndLora.bat
@@ -1,13 +1,7 @@
 @echo on
 pushd %~dp0SdWebUi-Installer\
 
-for /r .\Model %%b in (*.bat) do (
-	call %%b
-	timeout /t 10 /NOBREAK
-)
-for /r .\Lora %%b in (*.bat) do (
-	call %%b
-	timeout /t 10 /NOBREAK
-)
+for /r .\Model %%b in (*.bat) do ( call %%b )
+for /r .\Lora %%b in (*.bat) do ( call %%b )
 
 popd
