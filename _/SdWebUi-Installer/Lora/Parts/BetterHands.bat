e1e8c53c3a2b42209768402579a96bb059183df1
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-04-28 21:15:18 +0900

    dir bug fix

--- _/SdWebUi-Installer/Lora/Parts/BetterHands.bat
+++ _/SdWebUi-Installer/Lora/Parts/BetterHands.bat
@@ -3,8 +3,8 @@ pushd %~dp0..\..\..\..\SdWebUi\Lora\
 
 echo BetterHands.safetensors: https://civitai.com/models/47085/envybetterhands-locon
 if not exist Parts\_\ ( mkdir Parts\_\ )
-if not exist BetterHands.safetensors (
-	curl -Lo BetterHands.safetensors^
+if not exist Parts\_\BetterHands.safetensors (
+	curl -Lo Parts\_\BetterHands.safetensors^
 	https://civitai.com/api/download/models/55199
 	timeout /t 10 /nobreak
 )
--- _/SdWebUi-Installer/Lora/Parts/BlankEyes.bat
+++ _/SdWebUi-Installer/Lora/Parts/BlankEyes.bat
@@ -3,8 +3,8 @@ pushd %~dp0..\..\..\..\SdWebUi\Lora\
 
 echo BlankEyes.safetensors: https://civitai.com/models/33442/surprisedblankeyes
 if not exist Parts\_\ ( mkdir Parts\_\ )
-if not exist BlankEyes.safetensors (
-	curl -Lo BlankEyes.safetensors^
+if not exist Parts\_\BlankEyes.safetensors (
+	curl -Lo Parts\_\BlankEyes.safetensors^
 	https://civitai.com/api/download/models/49769
 	timeout /t 10 /nobreak
 )
