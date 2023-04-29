2c0fd7370ca58daa12a3e871bc19bacb7c31bb50
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-04-30 02:18:58 +0900

    Pose

--- _/SdWebUi-Installer/Lora/Pose/ChokeHold.bat
+++ _/SdWebUi-Installer/Lora/Pose/ChokeHold.bat
@@ -2,9 +2,9 @@
 pushd %~dp0..\..\..\..\SdWebUi\Lora\
 
 echo ChokeHold.safetensors: https://civitai.com/models/53592/concept-choke-hold
-if not exist Z-Pose-Z\ ( mkdir Z-Pose-Z\ )
-if not exist Z-Pose-Z\ChokeHold.safetensors (
-	curl -Lo Z-Pose-Z\ChokeHold.safetensors^
+if not exist Pose\ ( mkdir Pose\ )
+if not exist Pose\ChokeHold.safetensors (
+	curl -Lo Pose\ChokeHold.safetensors^
 	https://civitai.com/api/download/models/57944
 	timeout /t 10 /nobreak
 )
