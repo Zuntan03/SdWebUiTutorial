9f0ed311ea9dcc54db520d6be0e559f8f0f38e43
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-04-30 21:17:48 +0900

    bugfix

--- _/SdWebUi-Installer/Lora/Situ/Giantess.bat
+++ _/SdWebUi-Installer/Lora/Situ/Giantess.bat
@@ -1,10 +1,10 @@
 @echo off
 pushd %~dp0..\..\..\..\SdWebUi\Lora\
 
-echo Giantess .safetensors: https://civitai.com/models/25306/giantess-or-concept
+echo Giantess.safetensors: https://civitai.com/models/25306/giantess-or-concept
 if not exist Situ\ ( mkdir Situ\ )
-if not exist Situ\Giantess .safetensors (
-	curl -Lo Situ\Giantess .safetensors^
+if not exist Situ\Giantess.safetensors (
+	curl -Lo Situ\Giantess.safetensors^
 	https://civitai.com/api/download/models/40541
 	timeout /t 10 /nobreak
 )
