230377c5300701e71f8bc59bb91a5bb6f5423012
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-05-01 17:33:07 +0900

    MixPro bugfix

--- _/SdWebUi-Installer/Model/Anime/MixPro.bat
+++ _/SdWebUi-Installer/Model/Anime/MixPro.bat
@@ -5,7 +5,7 @@ echo MixPro.safetensors: https://civitai.com/models/7241/mix-pro-v4
 if not exist Anime\ ( mkdir Anime\ )
 if not exist Anime\MixPro.safetensors (
 	curl -Lo Anime\MixPro.safetensors^
-	https://civitai.com/api/download/models/34559
+	https://civitai.com/api/download/models/34559?type=Model&format=SafeTensor&size=full&fp=fp16
 )
 
 popd
