d75c0de71ba482990d149f02f9631e2aa3cd83b1
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-05-03 14:19:51 +0900

    add lora

---
+++ _/SdWebUi-Installer/Lora/zParts_/Tanliner.bat
@@ -0,0 +1,12 @@
+@echo off
+pushd %~dp0..\..\..\..\SdWebUi\Lora\
+
+echo Tanliner.safetensors: https://civitai.com/models/14673/vs-tanliner
+if not exist zParts_\ ( mkdir zParts_\ )
+if not exist zParts_\Tanliner.safetensors (
+	curl -Lo zParts_\Tanliner.safetensors^
+	https://civitai.com/api/download/models/22764
+	timeout /t 3 /nobreak
+)
+
+popd
