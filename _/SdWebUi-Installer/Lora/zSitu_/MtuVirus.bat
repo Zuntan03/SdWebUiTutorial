715040ac3ca439c53c0da6a804d2d77e88a14b72
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-05-04 17:30:14 +0900

    add LoRA

---
+++ _/SdWebUi-Installer/Lora/zSitu_/MtuVirus.bat
@@ -0,0 +1,12 @@
+@echo off
+pushd %~dp0..\..\..\..\SdWebUi\Lora\
+
+echo MtuVirus.safetensors: https://civitai.com/models/57573/mtu-virus
+if not exist zSitu_\ ( mkdir zSitu_\ )
+if not exist zSitu_\MtuVirus.safetensors (
+	curl -Lo zSitu_\MtuVirus.safetensors^
+	https://civitai.com/api/download/models/61999
+	timeout /t 3 /nobreak
+)
+
+popd
