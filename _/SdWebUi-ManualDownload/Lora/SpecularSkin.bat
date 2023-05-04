25b7163391fd0e818a919b57d9c21eb17ccadb43
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-05-04 11:50:52 +0900

    lora rename

--- _/SdWebUi-ManualDownload/Lora/SpecularSkin.bat
+++ _/SdWebUi-ManualDownload/Lora/SpecularSkin.bat
@@ -1,8 +1,8 @@
 @echo off
 pushd %~dp0..\..\..\SdWebUi\Lora\
 
-echo Specular: https://mega.nz/folder/8SdxnQLS#CJ7d7S2lgYjq6DjZG-jMgg/file/pSkQ3BDT
-echo specular_v2 ^> Filter\Specular
+echo SpecularSkin: https://mega.nz/folder/8SdxnQLS#CJ7d7S2lgYjq6DjZG-jMgg/file/pSkQ3BDT
+echo specular_v2 ^> Parts\SpecularSkin
 
 start "" https://mega.nz/folder/8SdxnQLS#CJ7d7S2lgYjq6DjZG-jMgg/file/oL0GgRQA
 
