d1e47900bf4a20baa74ae685846cdbde686b298c
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-05-04 11:33:47 +0900

    Add LoRA

---
+++ _/SdWebUi-Installer/Lora/Parts/HohoAka.bat
@@ -0,0 +1,17 @@
+@echo off
+pushd %~dp0..\..\..\..\SdWebUi\Lora\
+
+echo HohoAka.safetensors: https://huggingface.co/2vXpSwA7/iroiro-lora
+if not exist Parts\ ( mkdir Parts\ )
+if not exist Parts\HohoAka.safetensors (
+	curl -Lo Parts\HohoAka.safetensors^
+	https://huggingface.co/2vXpSwA7/iroiro-lora/resolve/main/test2/hohoaka.safetensors
+)
+
+if not exist Parts\HohoAka.jpg (
+	curl -Lo Parts\HohoAka.jpg^
+	https://i.imgur.com/uPbqqCn.jpg
+	timeout /t 3 /nobreak
+)
+
+popd
